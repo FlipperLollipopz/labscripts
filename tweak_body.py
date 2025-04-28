50fcf0dfb72dc625f12299a6a7661ca5f8529d31
Filippo Basilico <filippo@DebFlipper>
2025-04-28 16:36:07 +0200

    modified body script

--- tweak_body.py
+++ tweak_body.py
@@ -3,4 +3,4 @@ from mitmproxy import http
 def response(flow: http.HTTPFlow):
     ct = flow.response.headers.get("Content-Type", "")
     if "text" in ct:
-        flow.response.text = flow.response.text + "\n, You have been tricked!!!"
+        flow.response.text = flow.response.text + ", You have been tricked!!!"
